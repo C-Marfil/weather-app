94cd4406488e90a42792275372fad3f777ac904c
Carlos Marfil <carlos.marfil.r@gmail.com>
2023-01-30 22:36:04 +0000

    added Welcome pomodoro

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -7,7 +7,7 @@ import ReactPlayer from "react-player";
 
 const rain = "https://youtu.be/nF1fSKEiyA0";
 const clear = "https://youtu.be/YdtBEFK_HgI";
-const welcome = "https://youtu.be/IxOsgxSNN24";
+const welcome = "https://youtu.be/YdtBEFK_HgI";
 const snow = "https://youtu.be/UglAgexl_o8";
 const clouds = "https://youtu.be/grBFMP3HDZA";
 let url = "";
